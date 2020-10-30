04bbe8efa740502f1241644f4a533ad07ce72277
Roenski <aleksi.korsman@aalto.fi>
2020-10-30 22:27:22 +0200

    Add shell script for building the project

---
+++ Vivado/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Make sure you run this in the folder you wish to have your project in.
+
+# Insert the path where the vivado binary is located.
+VIVADO_PATH="/media/aleksi/HDDLnx/XilinxInstall/Vivado/2020.1/bin"
+$VIVADO_PATH/vivado -mode batch -source build.tcl
